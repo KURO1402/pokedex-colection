5f0053bfb889a2fa4a2206f131d4d19f6e54cf67
PabloDev <yauripablo70@gmail.com>
2025-12-01 18:54:38 -0500

    Cambiando conexion a supabase

--- servidor/src/config/db.js
+++ servidor/src/config/db.js
@@ -6,9 +6,11 @@ dotenv.config();
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  },
-  family: 4 
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
+  family: 4,
 });
